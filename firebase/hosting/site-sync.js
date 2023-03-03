16f89b5909e0cc27d4da18730bfbf0c84dfede86
Maksym Trykur <maksym@monk.io>
2023-03-03 19:39:08 +0200

    [chore] update firebase entities

--- firebase/hosting/site-sync.js
+++ firebase/hosting/site-sync.js
@@ -7,28 +7,39 @@ let createSite = function (def) {
         type: "USER_SITE"
     };
 
-    return gcp.post(BASE_URL + "/projects/" + def["project-id"] +
+    return gcp.post(BASE_URL + "/projects/" + def["project"] +
         "/sites?siteId=" + def["name"],
         {"body": JSON.stringify(body)});
 };
 
 let getSite = function (def) {
-    return gcp.get(BASE_URL + "/projects/" + def["project-id"] +
-        +"/sites/" + def["name"]);
+    return gcp.get(BASE_URL + "/projects/" + def["project"] +
+        "/sites/" + def["name"]);
 };
 
 let deleteSite = function (def) {
-    return gcp.delete(BASE_URL + "/projects/" + def["project-id"] +
-        +"/sites/" + def["name"]);
+    return gcp.delete(BASE_URL + "/projects/" + def["project"] +
+        "/sites/" + def["name"]);
 };
 
 function main(def, state, ctx) {
     let res = {};
     switch (ctx.action) {
         case "create":
+            let exRes = getSite(def);
+            if (!exRes.error) {
+                // use existing website
+                res = exRes;
+                break;
+            }
             res = createSite(def);
             break;
         case "purge":
+            res = getSite(def);
+            if (res.error && res.error.includes("response code 404")) {
+                // resource is removed
+                return;
+            }
             res = deleteSite(def);
             break;
         default:
@@ -36,7 +47,7 @@ function main(def, state, ctx) {
             return;
     }
     if (res.error) {
-        throw new Error(res.error);
+        throw new Error(res.error + ", body " + res.body);
     }
     // set instance result to state
     let body = JSON.parse(res.body)
