db4a5ce5831141b6bb73afc30bb56d2d33880a36
Burakhan <burakhanozyol@gmail.com>
2023-03-19 02:23:28 +0300

    added endpoint, port and status in state

--- aws/rds/rds.js
+++ aws/rds/rds.js
@@ -2,6 +2,7 @@ let aws = require("cloud/aws");
 let cli = require("cli");
 let http = require("http");
 let secret = require("secret");
+const parser = require("parser");
 
 function getDateString() {
     let date = new Date();
@@ -14,30 +15,29 @@ function getDateString() {
     return year + "" + month + "" + day + "-" + hour + "" + minute + "" + second;
 }
 function encodeQueryData(data) {
-    
     const ret = [];
     for (let d in data)
-      ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
+        ret.push(encodeURIComponent(d) + '=' + encodeURIComponent(data[d]));
     return ret.join('&');
- }
+}
 
 let createRDS = function (def) {
     cli.output("createRDS");
     data = {
         "headers": {
             "Content-Type": "application/x-amz-json-1.0"
-        }, 
+        },
         "service": "rds",
         "region": def["region"]
     };
     let password = "";
     try {
         password = secret.get(def["password-secret"]);
-      } catch (error) {
+    } catch (error) {
         // generate password and save to secret if it doesn't exist
         secret.set(def["password-secret"], secret.randString(16));
         password = secret.get(def["password-secret"]);
-      }
+    }
 
     let param = {
         "DBInstanceIdentifier": def["identifier"],
@@ -49,19 +49,28 @@ let createRDS = function (def) {
         "Version": "2014-09-01",
         "SignatureVersion": def["signature-version"],
         "SignatureMethod": def["signature-method"]
-        
+
     }
 
     const searchParams = encodeQueryData(param);
     cli.output("https://rds." + def["region"] + ".amazonaws.com/?Action=CreateDBInstance&" + searchParams.toString());
     // Create aws rds instance
     let res = aws.post("https://rds." + def["region"] + ".amazonaws.com/?Action=CreateDBInstance&" + searchParams.toString(), {
-            "headers": data.headers,
-            "service": data.service,
-            "region": data.region  
-        });
+        "headers": data.headers,
+        "service": data.service,
+        "region": data.region
+    });
 
-    return res
+
+    if (res.error) {
+        errorMessage = parser.xmlquery(res.body, "//ErrorResponse/Error/Message");
+        throw new Error(res.error + ", " + errorMessage);
+    }
+    let state = {
+        "status": parser.xmlquery(res.body, "//CreateDBInstanceResponse/CreateDBInstanceResult/DBInstance/DBInstanceStatus")[0],
+        "arn": parser.xmlquery(res.body, "//CreateDBInstanceResponse/CreateDBInstanceResult/DBInstance/DBInstanceArn"),
+    }
+    return state
 }
 
 let createSnapshot = function (def) {
@@ -69,7 +78,7 @@ let createSnapshot = function (def) {
     data = {
         "headers": {
             "Content-Type": "application/x-amz-json-1.0"
-        }, 
+        },
         "service": "rds",
         "region": def["region"]
     };
@@ -86,10 +95,10 @@ let createSnapshot = function (def) {
     cli.output("https://rds." + def["region"] + ".amazonaws.com/?Action=CreateDBSnapshot&" + searchParams.toString());
     // Create aws rds instance
     let res = aws.post("https://rds." + def["region"] + ".amazonaws.com/?Action=CreateDBSnapshot&" + searchParams.toString(), {
-            "headers": data.headers,
-            "service": data.service,
-            "region": data.region  
-        });
+        "headers": data.headers,
+        "service": data.service,
+        "region": data.region
+    });
 
     return res
 }
@@ -99,7 +108,7 @@ let deleteRDS = function (def) {
     data = {
         "headers": {
             "Content-Type": "application/x-amz-json-1.0"
-        }, 
+        },
         "service": "rds",
         "region": def["region"]
     };
@@ -114,15 +123,21 @@ let deleteRDS = function (def) {
     cli.output("https://rds." + def["region"] + ".amazonaws.com/?Action=DeleteDBInstance&" + searchParams.toString());
     // Create aws rds instance
     let res = aws.post("https://rds." + def["region"] + ".amazonaws.com/?Action=DeleteDBInstance&" + searchParams.toString(), {
-            "headers": data.headers,
-            "service": data.service,
-            "region": data.region  
-        });
-    
-    secret.remove(def["password-secret"]);
+        "headers": data.headers,
+        "service": data.service,
+        "region": data.region
+    });
 
-    return res
-    
+    try {
+        secret.remove(def["password-secret"]);
+    } catch (error) {
+        cli.output("Secret not found");
+    }
+
+    return {
+        "status": "deleted"
+    }
+
 }
 
 let getRDS = function (def) {
@@ -130,7 +145,7 @@ let getRDS = function (def) {
     data = {
         "headers": {
             "Content-Type": "application/x-amz-json-1.0"
-        }, 
+        },
         "service": "rds",
         "region": def["region"]
     };
@@ -144,13 +159,23 @@ let getRDS = function (def) {
     cli.output("https://rds." + def["region"] + ".amazonaws.com/?Action=DescribeDBInstances&" + searchParams.toString());
     // Create aws rds instance
     let res = aws.post("https://rds." + def["region"] + ".amazonaws.com/?Action=DescribeDBInstances&" + searchParams.toString(), {
-            "headers": data.headers,
-            "service": data.service,
-            "region": data.region  
-        });
+        "headers": data.headers,
+        "service": data.service,
+        "region": data.region
+    });
 
-    return res
-    
+    if (res.error) {
+        errorMessage = parser.xmlquery(res.body, "//ErrorResponse/Error/Message");
+        throw new Error(res.error + ", " + errorMessage);
+    }
+    let state = {
+        "status": parser.xmlquery(res.body, "//DescribeDBInstancesResponse/DescribeDBInstancesResult/DBInstances/DBInstance/DBInstanceStatus")[0],
+        "endpoint": parser.xmlquery(res.body, "//DescribeDBInstancesResponse/DescribeDBInstancesResult/DBInstances/DBInstance/Endpoint/Address")[0],
+        "port": parser.xmlquery(res.body, "//DescribeDBInstancesResponse/DescribeDBInstancesResult/DBInstances/DBInstance/Endpoint/Port")[0],
+        "password-secret": def["password-secret"]
+    }
+    return state
+
 }
 
 let showPassword = function (def) {
@@ -158,7 +183,7 @@ let showPassword = function (def) {
     let password = "";
     try {
         password = secret.get(def["password-secret"]);
-        } catch (error) {
+    } catch (error) {
         // generate password and save to secret if it doesn't exist
         throw new Error("Password secret not found");
     }
@@ -172,27 +197,31 @@ function main(def, state, ctx) {
     switch (ctx.action) {
         case "recreate":
         case "create":
-            createRDS(def)
+            state = createRDS(def)
             break;
         case "purge":
-            res = deleteRDS(def);
+            state = deleteRDS(def);
             break;
         case "create-snapshot":
             res = createSnapshot(def);
             break;
         case "get":
-            res = getRDS(def);
+            state = getRDS(def);
             break;
         case "show-password":
             res = showPassword(def);
             break;
+        case "check-readiness": {
+            state = getRDS(def);
+            if (state.status !== "available") {
+                throw new Error("RDS instance is not ready yet");
+            }
+            break;
+        }
         default:
             // no action defined
             return;
     }
-    if (res.error) {
-        throw new Error(res.error + ", body " + res.body);
-    }
 
-    return res.body;
+    return state;
 }
